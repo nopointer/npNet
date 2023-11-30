0e177b9dc0a16bfbfc687c137338c9199b34fc53
nopointer <857508412@qq.com>
2023-11-30 16:39:20 +0800

    ####

--- app/src/main/java/demo/nopointer/npNet/MainApplication.java
+++ app/src/main/java/demo/nopointer/npNet/MainApplication.java
@@ -9,20 +9,13 @@ import android.support.multidex.MultiDex;
 public class MainApplication extends Application {
 
 
-    private static final int NOTIFICATION_TITLE_TYPE = 9;
-    private static final int NOTIFICATION_CONTENT_TYPE = 10;
-
 
     public static MainApplication mainApplication = null;
-
-    private Handler handler = new Handler();
 
     @Override
     public void onCreate() {
         super.onCreate();
         mainApplication = this;
-
-
     }
 
 
@@ -35,9 +28,5 @@ public class MainApplication extends Application {
     public static MainApplication getMainApplication() {
         return mainApplication;
     }
-
-
-
-
 
 }
